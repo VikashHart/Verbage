5bc9f54f5a95cbead713ebfc5a4c73d962b8baa2
Vikash Hart <vikashhart@gmail.com>
2018-05-30 14:13:55 -0400

    added gitignore

---
+++ Verbage/Models/UIColor+Hex+cgColor.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Hex+cgColor.swift
+//  Verbage
+//
+//  Created by C4Q on 5/2/18.
+//  Copyright © 2018 Vikash Hart. All rights reserved.
+//
+
+import Foundation
---
+++ Verbage/Services/LocalNotificationService.swift
@@ -0,0 +1,9 @@
+//
+//  LocalNotificationService.swift
+//  Verbage
+//
+//  Created by C4Q on 5/13/18.
+//  Copyright © 2018 Vikash Hart. All rights reserved.
+//
+
+import Foundation
